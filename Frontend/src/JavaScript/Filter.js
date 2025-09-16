bc576ccf2a533695447ec7cc03f8244d8e0b511e
Aryan raj <114879450+ARYAN-RAJ08@users.noreply.github.com>
2025-09-16 18:43:53 +0530

    Remove all files

--- Frontend/src/JavaScript/Filter.js
+++
@@ -1,6 +0,0 @@
-export function changeByClick() {
-    var FilterBySubject = document.querySelector('.FilterBySubject');
-    var down = document.querySelector('.down');
-    down.classList.toggle('rotateElement')
-    FilterBySubject.classList.toggle('changeHeight');
-}
